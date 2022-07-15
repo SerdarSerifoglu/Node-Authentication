f2f3c6f284f4e6ddfbe9f7d3ac30171ca37b11be
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2022-07-15 15:03:04 +0300

    mongo connection by .env file

--- server/index.js
+++ server/index.js
@@ -1,7 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO_DB_CONNECTION)
+    .then(() => {
+      console.log("Connected MongoDB");
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
 
 app.listen(4000, () => {
-  console.log(`Connected: ${4000}`);
+  connect();
+  console.log(`Connected: ${4000} port`);
 });
