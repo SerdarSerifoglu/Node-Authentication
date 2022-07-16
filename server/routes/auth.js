d3baad36c4d24706d6b1d5d738c00c557384ca89
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2022-07-16 13:53:29 +0300

    update signup endpoint

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,9 +3,13 @@ import AuthService from "../services/authService.js";
 
 const router = express.Router();
 
-router.post("/signup", async (req, res) => {
-  const user = await AuthService.insert(req.body);
-  res.json(user);
+router.post("/signup", async (req, res, next) => {
+  try {
+    const user = await AuthService.insert(req.body);
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default router;
